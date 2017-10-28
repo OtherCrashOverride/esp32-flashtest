2b538d67fe9694096dbe5319fb98f760d68250cb
OtherCrashOverride <OtherCrashOverride@users.noreply.github.com>
2017-10-28 03:21:33 -0500

    Initial commit.

--- main/main.c
+++ main/main.c
@@ -5,38 +5,40 @@
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "esp_partition.h"
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    return ESP_OK;
-}
 
 void app_main(void)
 {
     nvs_flash_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = "access_point_name",
-            .password = "password",
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
+    const esp_partition_t* part;
+	spi_flash_mmap_handle_t handle;
+    void* data;
+	esp_err_t err;
+
+	part = esp_partition_find_first(0x40, 1, NULL);
+	if (part == 0)
+	{
+		printf("esp_partition_find_first failed.\n");
+		abort();
+	}
+
+	printf("pre mmap heap=%x\n", esp_get_free_heap_size());
+
+	err = esp_partition_mmap(part, 0, part->size, SPI_FLASH_MMAP_DATA, (const void**)&data, &handle);
+	if (err!=ESP_OK)
+	{
+		printf("esp_partition_mmap failed. (%d)\n", err);
+		abort();
+	}
+
+	printf("post mmap heap=%x\n", esp_get_free_heap_size());
+
+	printf("mmap=%p\n", data);
+
+    while (true)
+    {
         vTaskDelay(300 / portTICK_PERIOD_MS);
     }
 }
-
